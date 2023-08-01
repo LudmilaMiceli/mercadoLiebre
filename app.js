5d621aaea90145456054e235875dc316a886b25c
Lula <ludmilamiceli@hotmail.com>
2023-08-01 00:09:08 -0300

    Iniciando formularios

--- app.js
+++ app.js
@@ -3,5 +3,7 @@ const app = express();
 const path = require('path')
 const PORT = 3030;
 app.use(express.static('public'))
-app.get('/',(req,res) => res.sendFile(path.join(__dirname,"views","home.html")))
-app.listen(PORT,() => console.log(`server running in http://localhost:${PORT}`))
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, "views", "home.html")))
+app.get('/register', (req, res) => res.sendFile(path.join(__dirname, "views", "register.html")));
+app.get('/login', (req, res) => res.sendFile(path.join(__dirname, "views", "login.html")));
+app.listen(PORT, () => console.log(`server running in http://localhost:${PORT}`))
